024354cf260f345e5990534b73389a9db2f308ed
HUNG <phong1724@gail.com>
2025-12-04 13:13:17 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-//route
+const express = require('express')
+const app = express()
+const port = 3000
+
 app.get('/tin-tuc', (req, res) => {
-    var a =1 ;
-    var b =2 ;
-    var c = a + b ;
-    res.send('Hello World!');
-});
-//127.0.0.1 - localhost
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
